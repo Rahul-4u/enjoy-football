c53713dd5616ee8305d871d2877973d94169042c
Rahul <rahulwp24@gmail.com>
2025-05-23 08:40:18 +0600

    Setup tours page and add banner section

---
+++ src/pages/Tours.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ToursBanner from '../components/tours/ToursBanner'
+
+export default function Tours() {
+  return (
+      <div>
+          <ToursBanner/>
+    </div>
+  )
+}
